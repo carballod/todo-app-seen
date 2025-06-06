08b97e50e541a508438565d084cfc2956800e691
Daniela Carballo <daniela.carballo@biggertech.co>
2025-06-06 16:06:19 -0300

    refactor: hace que app module se exporte para tests

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,11 @@ app.use(express.json());
 app.use("/api/tasks", tasksRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
